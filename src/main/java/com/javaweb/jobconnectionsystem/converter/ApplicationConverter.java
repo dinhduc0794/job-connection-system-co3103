159e84f638f681d73fdd798fa5475b1ddf144db9
dinhduc0794 <ngdduc79@gmail.com>
2024-12-04 14:43:13 +0700

    Update ApplicationConverter - save Application to Applicant

--- src/main/java/com/javaweb/jobconnectionsystem/converter/ApplicationConverter.java
+++ src/main/java/com/javaweb/jobconnectionsystem/converter/ApplicationConverter.java
@@ -1,6 +1,8 @@
 package com.javaweb.jobconnectionsystem.converter;
 
+import com.javaweb.jobconnectionsystem.entity.ApplicantEntity;
 import com.javaweb.jobconnectionsystem.entity.ApplicationEntity;
+import com.javaweb.jobconnectionsystem.entity.JobPostingEntity;
 import com.javaweb.jobconnectionsystem.model.dto.ApplicationDTO;
 import com.javaweb.jobconnectionsystem.repository.ApplicantRepository;
 import com.javaweb.jobconnectionsystem.repository.JobPostingRepository;
@@ -19,10 +21,14 @@ public class ApplicationConverter {
     public ApplicationEntity toApplicationEntity(ApplicationDTO applicationDTO){
         ApplicationEntity applicationEntity = modelMapper.map(applicationDTO, ApplicationEntity.class);
         if(applicationDTO.getApplicantId()!=null) {
-            applicationEntity.setApplicant(applicantRepository.findById(applicationDTO.getApplicantId()).get());
+            ApplicantEntity applicantEntity = applicantRepository.findById(applicationDTO.getApplicantId()).get();
+            applicationEntity.setApplicant(applicantEntity);
+            applicantEntity.getApplications().add(applicationEntity);
         }
         if(applicationDTO.getJobPostingId()!=null) {
-            applicationEntity.setJobPosting(jobPostingRepository.findById(applicationDTO.getJobPostingId()).get());
+            JobPostingEntity jobPostingEntity = jobPostingRepository.findById(applicationDTO.getJobPostingId()).get();
+            applicationEntity.setJobPosting(jobPostingEntity);
+            jobPostingEntity.getApplications().add(applicationEntity);
         }
         return applicationEntity;
     }
