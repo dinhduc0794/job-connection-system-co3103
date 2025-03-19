1542c2223dc4673146cbbba310f31de6af5bff62
dinhduc0794 <ngdduc79@gmail.com>
2025-03-19 22:22:40 +0700

    Hot fix

--- src/main/java/com/javaweb/jobconnectionsystem/entity/BaseEntity.java
+++ src/main/java/com/javaweb/jobconnectionsystem/entity/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @Column(name = "created_at", updatable = false) // Chỉ thiết lập khi tạo mới
     private LocalDateTime createdAt;
 
